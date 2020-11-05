81cc6969f550a7919acf0e6b1d549975c3579bfa
adm-sabitovka <sabitovka@shkd.bizml.ru>
2020-11-05 09:45:40 +0300

    Генерация списка тестов

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 import generateHeader from "./header.js"
+import generateQuizesPage from './quizesPage.js'
+import './_load.js'
 
 generateHeader();
+generateQuizesPage();
---
+++ js/quizesPage.js
@@ -0,0 +1,39 @@
+import getData from './data.js'
+
+const generateQuizesPage = () => {
+
+  const quizes = document.querySelector('.quizes');
+
+  const generateCard = (data) => {
+    quizes.textContent = '';
+
+    let contentHTML = '';
+
+    data.forEach(item => {
+      contentHTML += `
+        <div class="col s12 m6">
+          <div class="card horizontal">
+            <div class="card-stacked">
+              <div class="card-content">
+                <p>${item.title}</p>
+              </div>
+              <div class="card-action">
+                <a href="/quiz.html#${item.id}">Начать тест</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      `
+    })
+
+    quizes.insertAdjacentHTML('afterbegin', contentHTML);
+  }
+
+
+  if (location.pathname.startsWith('/index') || location.pathname === '/') {
+    getData.quizesList(generateCard);
+  }
+
+}
+
+export default generateQuizesPage;
