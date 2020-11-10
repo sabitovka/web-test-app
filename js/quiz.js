110de728965d66dc4e41a22f4907790b3a39cfb9
adm-sabitovka <sabitovka@shkd.bizml.ru>
2020-11-10 11:18:43 +0300

    Добавлен сбор ответов на вопросы

--- js/quiz.js
+++ js/quiz.js
@@ -1,20 +1,57 @@
-import { isAutorized } from './autorization.js' 
+import { User } from './autorization.js' 
 import getData from './data.js';
 
 const generateQuiz = () => {
 
-
-  const handleQuiz = ({ title, questions }) => {
+  const handleQuiz = ({ id, title, questions }) => {
 
     const quizTitle = document.querySelector('.quiz-title');
     const nextBtn = document.querySelector('.next-btn');
+    const nextBtnLink = document.querySelector('.next-btn');
 
     let currentQuestion = 0;
 
     quizTitle.textContent = title;
-    renderQuestion(questions[1]);
+    renderQuestion(questions[currentQuestion++]);
 
-    nextBtn.addEventListener('click', () => renderQuestion(questions[currentQuestion++]))
+    const userAnswers = {
+      id: id,
+      name: User.name,
+      group: User.group,
+      results: [],
+    };
+
+    const saveAnswer = () => {
+      const isMulti = questions[currentQuestion-1].type === 'multiChoise';
+      let result =  !isMulti ? 0 : [];
+      document.forms[0]
+        .querySelectorAll(`input${questions[currentQuestion-1].type !== 'text' ? ':checked' : ''}`)
+        .forEach(item => {
+          if (!isMulti)
+            result = item.value;
+          else {
+            result.push(item.value);
+          }
+        });
+      userAnswers.results.push(result);
+      console.log(userAnswers);
+    }
+
+
+    const handleNext = () => {
+      if (currentQuestion + 1 >= questions.length) {
+        nextBtn.textContent = 'Завершить';
+        nextBtn.removeEventListener('click', handleNext);
+        // todo сохранить результаты в localstorage
+        //nextBtn.addEventListener('click', () => document.location.href = '/result.html');
+      }
+      
+      renderQuestion(questions[currentQuestion++])
+    }
+
+    nextBtn.addEventListener('click', saveAnswer);
+    nextBtn.addEventListener('click', handleNext);
+    
   }
 
   const renderQuestion = ({ title, img, type, variants }) => {    
@@ -25,11 +62,12 @@ const generateQuiz = () => {
     let quizInputHTML = '';
 
     if (type !== 'text') {
+      let index = 0;
       variants.forEach(item => {
         quizInputHTML += `
           <li class="collection-item">
             <label>
-              <input name="group1" type="${type === 'multiChoise' ? 'checkbox' : 'radio'}"/>
+              <input name="group1" type="${type === 'multiChoise' ? 'checkbox' : 'radio'}" value="${index++}"/>
               <span>${item}</span>
             </label>
           </li>
@@ -47,7 +85,7 @@ const generateQuiz = () => {
     }
 
     contentHTML = `
-      <div class="question">
+      <form class="question">
         ${img ? `
         <div class="card-panel">
           <img src=${img} alt="" width="100%" height="100%" >
@@ -56,7 +94,7 @@ const generateQuiz = () => {
         <ul class="quiz-input collection ">
           ${quizInputHTML}
         </ul>
-      </div>
+      </form>
     `
 
     
@@ -66,7 +104,7 @@ const generateQuiz = () => {
 
   }
 
-  if (location.hash && location.pathname.includes('quiz') && isAutorized()) {
+  if (location.hash && location.pathname.includes('quiz') && User.isAutorized()) {
     getData.quiz(location.hash.substr(1), handleQuiz);
   }
 
