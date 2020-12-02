2ad9b3a9ee8be80fae52dbc98a3cccd918de1e6f
adm-sabitovka <sabitovka@shkd.bizml.ru>
2020-12-02 09:10:27 +0300

    Страница о тесте

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@
 
 import generateHeader from "./header.js"
 import generateQuizesPage from './quizesPage.js'
+import generateAboutPage from './aboutTest.js'
 import generateQuiz from './quiz.js'
 import generateResults from './result.js'
 import './_load.js'
 
 generateHeader();
- generateQuizesPage();
+generateQuizesPage();
+generateAboutPage();
 // generateQuiz();
 // generateResults();
