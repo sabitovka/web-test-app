31a0c4b74f3e5e507cb065d9a2923ca543df078e
adm-sabitovka <sabitovka@shkd.bizml.ru>
2021-09-28 09:16:21 +0300

    Добавлен файл .htaccess и index.php для роутинга
    Настроено перенаправление url вида localhost/admin/user/1 на index.php?q=admin/user/1
    В файле index.php определяется метод и вызывается функция роутинга по соответсвующему названию

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+$method = $_SERVER["REQUEST_METHOD"];
+$formData = getFormData($method);
+
+$url = (isset($_GET['q'])) ? $_GET['q'] : '';
+$url = rtrim($url, '/');
+$urls = explode('/', $url);
+
+$router = $urls[0];
+$urlData = array_slice($urls, 1);
+
+include_once 'routes/' . $router . '.php';
+route($method, $urlData, $formData);
+
+function getFormData($method) {
+  if ($method === 'GET') return $_GET;
+  if ($method === 'POST') return $_POST;
+
+  // TODO 23.09.2021 8:56: Добавить методы put patch delete
+}
+
+?>
