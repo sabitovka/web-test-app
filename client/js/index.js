b1c914697e74b5ff4c2ecef642b58a82c4a54711
adm-sabitovka <sabitovka@shkd.bizml.ru>
2021-10-25 14:46:50 +0300

    Добавлены файлы клиента

---
+++ client/js/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import generateHeader from "./header.js"
+import generateQuizesPage from './quizesPage.js'
+import generateAboutPage from './aboutTest.js'
+import generateQuiz from './quiz.js'
+import generateResults from './result.js'
+
+generateHeader();
+generateQuizesPage();
+generateAboutPage();
+generateQuiz();
+generateResults();
---
+++ client/js/result.js
@@ -0,0 +1,71 @@
+import getData from './data.js';
+import { getScoreECTS, getScore, loadResults } from './resultParser.js'
+
+const generateResults = () => {
+  
+  
+  const renderResult = (name, group, id) => {
+    const testTitle = document.querySelector('.test-title');
+    const scoreSpan = document.querySelector('.score-result :first-child')
+    const scoreElem = document.querySelector('.score-result :nth-child(2)');
+    const ects = document.querySelector('.score-result :last-child');
+    const resSpan = document.querySelector('.about-result b');
+    const resultWrapper = document.querySelector('.result');
+    const canvas = document.getElementById('chart').getContext('2d');
+    const resultLink = document.querySelector('.result-link a');
+    const resultLinkButton = document.querySelector('.result-link button');
+    const resultLinkInput = document.querySelector('.result-link input');
+    const btnTryAgain = document.querySelector('.btn-try-again');
+
+    const res = loadResults(name, group, id);
+    if (!res) {
+      return;
+    }
+
+    let rightCount = res.resultMask.filter(item => item === true).length;
+    let allCount = res.resultMask.length;
+    let score = getScore(rightCount, allCount);
+
+    testTitle.textContent = res.title;
+    resultWrapper.classList.remove('d-none');
+    scoreSpan.textContent =  score <= 59 ? 'не' : '';
+    resSpan.textContent = `${rightCount} из ${allCount}`;
+    scoreElem.textContent = score;
+    ects.textContent = getScoreECTS(score);
+
+    resultLink.setAttribute('href', location.href);
+    resultLink.textContent = location.href;
+    resultLinkInput.value = location.href;
+
+    resultLinkButton.addEventListener('click', () => {
+      resultLinkInput.select();
+      document.execCommand("copy");
+    })
+    btnTryAgain.setAttribute('href', `./test.html#${id}`)
+    new Chart(canvas, {
+      type: 'pie',
+      data: {
+        labels: ['Правильных', 'Неправильных'],
+        datasets: [{
+          label: 'Результаты теста',
+          backgroundColor: ['green', 'darkred' ],
+          borderColor: 'black',
+          data: [rightCount, allCount-rightCount]
+        }]
+      }
+    });
+
+  }
+  
+  if (location.search && (location.pathname.startsWith('/Web-Test-App/result') || location.pathname.startsWith('/result'))) {
+    const search = decodeURI(location.search).slice(1).split('&');
+    const name = search[0].split('=')[1];
+    const group = search[1].split('=')[1];
+    const id = search[2].split('=')[1];
+    console.log(search, name, group, id);
+    renderResult(name, group, id);
+  }
+
+}
+
+export default generateResults;
